7b5745f7456a9c5eb0d61fe88c2b2fe5f6d8d5ee
aclcoelho <ana.lunardello@gmail.com>
2022-09-27 10:17:54 -0300

    Altering files to match current version

--- rss_feed_reader/rss/routes.py
+++
@@ -1,8 +0,0 @@
-from turtle import title
-from flask import render_template, Blueprint
-
-rss = Blueprint('rss', __name__)
-
-@rss.route("/rss_feed")
-def rss_feed():
-    return render_template("feed.html")
