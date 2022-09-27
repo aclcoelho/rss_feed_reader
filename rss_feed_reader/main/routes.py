067a59943b23df251bcd2ec54096eb7cb364da2b
aclcoelho <ana.lunardello@gmail.com>
2022-09-27 10:09:40 -0300

    Adding functionality to input form

--- rss_feed_reader/main/routes.py
+++ rss_feed_reader/main/routes.py
@@ -1,9 +1,14 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, redirect, url_for
 import feedparser
 
 main = Blueprint('main', __name__)
 
-@main.route("/")
-@main.route("/home")
+@main.route("/", methods=['GET', 'POST',])
+@main.route("/home",methods=['GET', 'POST',])
 def home():
+    if request.method == "POST":
+        url = request.form.get("url")
+        parsed_url = feedparser.parse(url)
+        for post in parsed_url.entries:
+            print(post['title'])
     return render_template("home.html")
--- rss_feed_reader/rss/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class URLForm(FlaskForm):
-    url = StringField('URL', validators=[DataRequired()])
-    submit = SubmitField('Post')
